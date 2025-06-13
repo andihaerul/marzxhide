74e955d01ca53528b5a1f9a0167b6ba2def5fbbc
andihaerul <andihaerulkelautan@gmail.com>
2025-06-14 02:52:34 +0800

    Create installer3.sh

---
+++ installer3.sh
@@ -0,0 +1,33 @@
+   #bin bash
+apt update -y
+apt-get install jq -y
+read -rp "domain (example:svr.hidessh.com) : " -e domain
+read -rp "username: " -e username
+read -rp "password: " -e password
+read -rp "kouta (format GB example 50): " -e kouta
+gb=1073741824
+usage=$((kouta * gb))
+echo $usage > kouta.txt
+echo $domain > domain.txt
+
+url=$domain/api/admin/token
+response=$(curl -X POST -d "username=$username&password=$password&grant_type=password&scope=" -H "Accept: application/json" $url)
+#echo $response
+token=$(echo $response | jq -r '.access_token')
+echo $token > token.txt
+
+#add on Vmess
+wget -O /usr/local/bin/add-vmess-user "https://raw.githubusercontent.com/andihaerul/Developers/main/marzxhide/gen-vmess.sh"
+wget -O /usr/local/bin/del-vmess-user "https://raw.githubusercontent.com/andihaerul/Developers/main/marzxhide/del-vmess.sh"
+
+#add on Trojan
+wget -O /usr/local/bin/add-trojan-user "https://raw.githubusercontent.com/andihaerul/Developers/main/marzxhide/gen-trojan.sh"
+wget -O /usr/local/bin/del-trojan-user "https://raw.githubusercontent.com/andihaerul/Developers/main/marzxhide/del-vmess.sh"
+
+#permission
+chmod +x /usr/local/bin/add-vmess-user
+chmod +x /usr/local/bin/del-vmess-user
+chmod +x /usr/local/bin/add-trojan-user
+chmod +x /usr/local/bin/del-trojan-user
+
+rm -rf installer3.sh
